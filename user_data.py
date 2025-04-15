f8e0566106fc24f8ab2abf9158284eceaafc821e
Hospitaller <siribn@yandex.ru>
2025-04-15 10:59:09 +0400

    Добавлено повторное чтение переменных окружения при добавлении, получении и удалении пользователей в классе UserData

--- user_data.py
+++ user_data.py
@@ -22,15 +22,18 @@ class UserData:
     def add_user(self, user_id: int, wb_token: str):
         env_key = f'WB_TOKEN_{user_id}'
         set_key(self.env_file, env_key, wb_token)
+        load_dotenv(self.env_file, override=True)
         self.data[user_id] = {
             'wb_token': wb_token,
             'auto_check_enabled': False
         }
 
     def get_user_token(self, user_id: int) -> Optional[str]:
+        load_dotenv(self.env_file, override=True)
         return os.getenv(f'WB_TOKEN_{user_id}')
 
     def is_user_exists(self, user_id: int) -> bool:
+        load_dotenv(self.env_file, override=True)
         return f'WB_TOKEN_{user_id}' in os.environ
 
     def set_auto_check_status(self, user_id: int, status: bool):
@@ -43,5 +46,6 @@ class UserData:
     def remove_user(self, user_id: int):
         env_key = f'WB_TOKEN_{user_id}'
         unset_key(self.env_file, env_key)
+        load_dotenv(self.env_file, override=True)
         if user_id in self.data:
             del self.data[user_id] 
